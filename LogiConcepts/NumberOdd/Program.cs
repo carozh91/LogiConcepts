d3d1b1faf05e0503b3576e8c6f36308b2debbdd2
CAROLINA ZAPATA HENAO <krola_91@hotmail.es>
2024-03-30 12:49:44 -0500

    Create my firts commit.

---
+++ LogiConcepts/NumberOdd/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.Design;
+var number = string.Empty;
+do
+{
+    Console.Write("Ingrese numero o presione 'salir' para salir: ");
+    number = Console.ReadLine();
+    if (number.ToLower() == "salir")
+    {
+        continue;
+    }
+      
+   
+    var numberN = 0;
+    if (int.TryParse(number, out numberN))
+    {
+        if (numberN % 2 == 0)
+        {
+            Console.WriteLine($"El número {numberN} es par");
+        }
+        else
+        {
+            Console.WriteLine($"El número {numberN} es impar");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Lo que ingresaste {number} no es un número");
+    }
+} while (number.ToLower() != "salir");
+Console.WriteLine("Game Over.");
+
